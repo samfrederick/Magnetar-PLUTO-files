02b9f5798304700b2b7fad33e9c98cf778a8a419
Sam Frederick <safrederick@davidson.edu>
2018-08-27 19:33:07 -0400

    Created init.c, included docstring

---
+++ init.c
@@ -0,0 +1,11 @@
+/*
+Author: Sam Frederick
+Date: 8-27-18
+
+Simulates a basic disk with radius q1 centered around the origin in a 2D
+Cartesian domain. Credit goes to the PLUTO User Manual, Chapter 5 Section 1,
+Example #1 for listing these initial conditions for spatial coordinates. 
+
+
+ */
+#include "pluto.h"
