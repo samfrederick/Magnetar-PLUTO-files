dcd1a92cba324d5f3fa6e0a6d5443ed920f5d1f9
Sam Frederick <safrederick@davidson.edu>
2018-08-27 19:45:15 -0400

    Added body of code

--- init.c
+++ init.c
@@ -4,8 +4,24 @@ Date: 8-27-18
 
 Simulates a basic disk with radius q1 centered around the origin in a 2D
 Cartesian domain. Credit goes to the PLUTO User Manual, Chapter 5 Section 1,
-Example #1 for listing these initial conditions for spatial coordinates. 
+Example #1 for listing these initial conditions for spatial coordinates.
 
 
  */
 #include "pluto.h"
+
+/* ******************************************************************* */
+void Init (double *v, double x1, double x2, double x3)
+{
+    double r;
+
+    r = sqrt(x1*x1 + x2*x2);
+    v[VX1] = v[VX2] = 0.0;
+    if (r < 1.0){
+      v[RHO] = 10.0;
+      v[PRS] = 30.0;
+    }else{
+      v[RHO] = 1.0;
+      v[PRS] = 1.0;
+    }
+}
