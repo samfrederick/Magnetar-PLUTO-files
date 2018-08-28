609ee4128896da9a25e215a1acde28467d2a5146
Sam Frederick <safrederick@davidson.edu>
2018-08-28 11:49:57 -0400

    Added pressure and density equations from n = 1 polytropic model.

--- init.c
+++ init.c
@@ -10,6 +10,9 @@ Example #1 for listing these initial conditions for spatial coordinates.
  */
 #include "pluto.h"
 
+#define RHO_C  10.0 /* Density at center of star */
+#define K  5.0 /* Constant for polytropic pressure profile */
+
 /* ******************************************************************* */
 void Init (double *v, double x1, double x2, double x3)
 {
@@ -17,9 +20,12 @@ void Init (double *v, double x1, double x2, double x3)
 
     /*r = sqrt((x1-0.5)*(x1-0.5)+(x2-0.5)*(x2-0.5)); /* magnitude of radius */
     v[VX1] = v[VX2] = 0.0; /* initilize vx and vy to zero */
-    if (x1 < 1.0){ /* interior to disk set pressure and density uniform vals */
-      v[RHO] = 10.0;
-      v[PRS] = 30.0;
+    if ((x1 < 1.0) && (x1 != 0)){ /* interior to disk set pressure and density uniform vals */
+      v[RHO] = (RHO_C*sin((CONST_PI*x1)/1.0)*1.0)/(x1*CONST_PI); /* Density for n = 1 polytrope*/
+      v[PRS] = K*v[RHO]*v[RHO]; /* Pressure for n = 1 polytrope*/
+    }else if(x1 = 0){
+      v[RHO] = RHO_C; /* Density at star core */
+      v[PRS] = K*v[RHO]*v[RHO];
     }else{  /* exterior to disk set pressure and density to unit vals */
       v[RHO] = 1.0;
       v[PRS] = 1.0;
