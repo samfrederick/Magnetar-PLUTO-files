a4e532133d7d20f0a8af6ba35947da4958dbb289
Sam Frederick <safrederick@davidson.edu>
2018-08-28 20:34:53 -0400

    Added polytropic model of pressure and density as well as potential.

--- init.c
+++ init.c
@@ -1,34 +1,43 @@
 /*
 Author: Sam Frederick
-Date: 8-27-18
+Created: 8-27-18
 
-Simulates a basic disk with radius q1 centered around the origin in a 2D
-Cartesian domain. Credit goes to the PLUTO User Manual, Chapter 5 Section 1,
-Example #1 for listing these initial conditions for spatial coordinates.
-
+Simulates a disk modeling a star with radius 10 centered around the origin in a
+2D cylindrical domain.
 
  */
 #include "pluto.h"
 
-#define RHO_C  10.0 /* Density at center of star */
-#define K  5.0 /* Constant for polytropic pressure profile */
+/* ******************************************************************** */
+/*!
+ * A Note on Constants:
+ * These constants are defined relative to the 'UNIT_DENSITY','UNIT_LENGTH',
+ * 'UNIT_VELOCITY' parameters in definitions.h. They are tentative, and will
+ * very likely be updated to reflect accurate values. Changing their value
+ * causes drastic variation in the stability of the resulting model. This
+ * emphasizes the importance of determining accurate values for these parameters.
+/* ******************************************************************** */
+#define RHO_C  1 /* Density at center of star */
+#define K  0.935 /* Constant for polytropic pressure profile */
+#define G  6e-8 /* Gravitational constant */
+#define M_STAR  1.e5 /* Mass of Star */
+#define R 10.0 /*Radius of Star*/
 
 /* ******************************************************************* */
 void Init (double *v, double x1, double x2, double x3)
 {
     double r;
 
-    /*r = sqrt((x1-0.5)*(x1-0.5)+(x2-0.5)*(x2-0.5)); /* magnitude of radius */
     v[VX1] = v[VX2] = 0.0; /* initilize vx and vy to zero */
-    if ((x1 < 1.0) && (x1 != 0)){ /* interior to disk set pressure and density uniform vals */
+    if ((x1 < 1.0) && (x1 != 0)){ /* set pressure and density inside disk */
       v[RHO] = (RHO_C*sin((CONST_PI*x1)/1.0)*1.0)/(x1*CONST_PI); /* Density for n = 1 polytrope*/
       v[PRS] = K*v[RHO]*v[RHO]; /* Pressure for n = 1 polytrope*/
-    }else if(x1 = 0){
+    }else if(x1 = 0){ /* Density at center, this may be causing errors in simulation */
       v[RHO] = RHO_C; /* Density at star core */
-      v[PRS] = K*v[RHO]*v[RHO];
+      v[PRS] = K*RHO_C*RHO_C;
     }else{  /* exterior to disk set pressure and density to unit vals */
-      v[RHO] = 1.0;
-      v[PRS] = 1.0;
+      v[RHO] = 1e-5;
+      v[PRS] = 1e-5;
     }
 }
 
@@ -37,7 +46,7 @@ void Init (double *v, double x1, double x2, double x3)
 /* ********************************************************************* */
 void InitDomain (Data *d, Grid *grid)
 /*!
- * Assign initial condition by looping over the computational domain.
+ * Assign initial condition by looping over the computatiohttps://photos.google.com/nal domain.
  * Called after the usual Init() function to assign initial conditions
  * on primitive variables.
  * Value assigned here will overwrite those prescribed during Init().
@@ -66,4 +75,26 @@ void UserDefBoundary (const Data *d, RBox *box, int side, Grid *grid)
  *
  *********************************************************************** */
 {
+}
+/*#if (BODY_FORCE & POTENTIAL)*/
+/* ************************************************************************ */
+double BodyForcePotential(double x1, double x2, double x3)
+/*
+ * A graviational poential is assigned to the problem. This potential comes
+ * from the work of Kuhn's Thesis, with a note to a minor typo to Kuhn's
+ * potential at r = 0 (referred to as phi nought).
+ *************************************************************************** */
+{
+  double phi;
+
+  if ((x1 < 1) && (x1 != 0)) { /* Potential interior to star except r = 0 */
+    phi = 4*G*RHO_C*(-(R*R*R*sin((CONST_PI*x1)/R))/(CONST_PI*CONST_PI*x1)-(M_STAR)/(4*R*RHO_C));
+  }
+  if (x1 >= 1){ /* Potential exterior to star */
+    phi = -(G*M_STAR) / x1;
+  }
+  if (x1 = 0){ /* Potential at r = 0 */
+    phi = 4*G*RHO_C*(-(R*R)/(CONST_PI)-(M_STAR)/(4*R*RHO_C));
+  }
+  return phi;
 }
