325d875c6c751f9992188f4352cc584448422cfc
Sam Frederick <safrederick@davidson.edu>
2018-10-15 09:51:58 -0400

    Added python file for analysis of pressure and density profiles

---
+++ Python Files/Pressure and Density Analysis/Density_and_Pressure_Analysis.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pylab as py
+
+RPOT = 1.857595e20 # CORRECTED VALUE FOR GM/R, was 6.67e19 prior to correction (likely
+# due to temporary M_STAR mass of 1.0e33)
+GPRSQ = 1.4674e20
+G_CONST = 6.67e-8
+M_STAR = 2.785e33
+RHO_C = 2.2e15
+R = 1.e6
+CONST_PI = 3.1415926
+VACUUM = 5e10
+K = 4.25e4
+
+UNIT_DENSITY = 1.e8
+UNIT_LENGTH = 1.e10
+UNIT_VELOCITY = 1.e10
+
+r = np.linspace(0,2,70)
+pressure = list()
+rho = list()
+
+for x1 in r:
+    if x1 < 1.0 and x1 != 0.0:
+        rhoval = (RHO_C*np.sin(CONST_PI*x1))/(x1*CONST_PI) +VACUUM
+        pressureval = K*rhoval*rhoval
+        
+    elif x1 == 0:
+        rhoval = RHO_C + VACUUM
+        pressureval = K*RHO_C*RHO_C
+        
+    elif x1 >= 1.0:
+        rhoval = VACUUM / UNIT_DENSITY;
+        pressureval = (K*VACUUM*VACUUM)/(UNIT_DENSITY*UNIT_VELOCITY*UNIT_VELOCITY);
+        
+    rhoval = rhoval / UNIT_DENSITY
+    pressureval = pressureval / (UNIT_DENSITY*UNIT_VELOCITY*UNIT_VELOCITY)  
+    
+    rho.append(rhoval)
+    pressure.append(pressureval) 
+    
+# py.plot(r,rho,"ro-")
+py.plot(r,pressure,"ro-")
+py.yscale("log")
+py.title("Log(Pressure) vs. Radius ")
+py.ylabel("Log(Pressure)")
+py.xlabel("Radius")
+py.show()
