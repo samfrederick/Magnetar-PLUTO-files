5b34302e6052e13beec12f06be0ebed6f6973870
Sam Frederick <safrederick@davidson.edu>
2019-08-09 07:59:03 -0400

    Cleaning up some code, copyright statement

--- Magnetar_Main/init.c
+++ Magnetar_Main/init.c
@@ -1,23 +1,15 @@
 /* ///////////////////////////////////////////////////////////////////// */
 /*!
-  \file
-  \brief Contains basic functions for problem initialization.
+   file: init.c
+   brief: Contains basic functions for problem initialization.
 
-  The init.c file collects most of the user-supplied functions useful
-  for problem configuration.
-  It is automatically searched for by the makefile.
+   The init.c file collects most of the user-supplied functions useful
+   for problem configuration.
+   It is automatically searched for by the makefile.
 
-  \ author: Sam Frederick, Davidson College '19
-  \ date created: 8-27-18, work in progress.
-*/
-/* ///////////////////////////////////////////////////////////////////// */
-
-#include "pluto.h"
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-
-/*
+   author: Sam Frederick, Davidson College '19
+   date created: 8-27-18 through 5-19-19
+   Copyright 2018, Sam Frederick, All rights reserved
 
 FOREWORD (9-7-18):
 
@@ -26,7 +18,7 @@ and are subsequently converted into 'dimensionless units' with considerations
 for minimizing computational over/underflow and in supporting the robustness of
 chosen numerical methods for computation.
 
-The radius r, which will be referred to in the code as 'x1' is a value that
+The radius r, referred to in the code as 'x1', is a value that
 ranges from {0 < r < C} where C is the outermost boundary of the computational
 domain specified in line 3 of the pluto.ini file. The domain of the star is
 defined by {0 < r < 1}, where R = 1 is the radius of the star. There are numerous
@@ -41,6 +33,11 @@ by 10000 to 'normalize' the ratio r / R such that (10000*x1)/ R = r / R.
 
 
 /* ********************************************************************* */
+
+#include "pluto.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
 /* PHYSICAL CONSTANTS */
 #define G_CONST  6.67e-8 /*cm^3 g^-1 s^-2 */        /* Gravitational constant */
@@ -233,134 +230,7 @@ void InitDomain (Data *d, Grid *grid)
 {
 
 }
-/* ********************************************************************* */
-// double simpsonzz (int no, double min, double max)
-// /*!
-// *  Perform simpsons approximation for integration of Izzint
-// * Credit given to the following for publicly posting code:
-// *								                                                       *
-// *  taken from: "Projects in Computational Physics" by Landau and Paez  *
-// *	       copyrighted by John Wiley and Sons, New York                  *
-// *                                                                      *
-// *  written by: students in PH465/565, Computational Physics,           *
-// *	       at Oregon State University                                    *
-// *              code copyrighted by RH Landau                           *
-// *  supported by: US National Science Foundation, Northwest Alliance    *
-// *                for Computational Science and Engineering (NACSE),    *
-// *                US Department of Energy 	                             *
-// *								                                                       *
-// *  UNIX (DEC OSF, IBM AIX): cc integ.c -lm                             *
-// *    			                                                             *
-// */
-// {
-// int n;
-// double interval, sum=0.0, x;
-//
-// interval = ((max-min))/(no-1);
-//
-// for (n=2; n<no; n+=2)           /* loop for odd points      */
-//    {
-//        x = interval * (n-1);
-//        sum += 4 * Izzint(x);
-//    }
-//    for (n=3; n<no; n+=2)        /* loop for even points     */
-//    {
-//       x = interval * (n-1);
-//       sum += 2 * Izzint(x);
-//    }
-//    sum +=  Izzint(min) + Izzint(max);	 	  /* add first and last value */
-//    sum *= interval/3.;        	/* then multilpy by interval*/
-//
-//    return (sum);
-//
-// }
-// /* ********************************************************************* */
-// double simpsonxx (int no, double min, double max)
-// /*!
-// *  Perform simpsons approximation for integration of Ixxint
-// * Credit given to the following for publicly posting code:
-// *								                                                       *
-// *  taken from: "Projects in Computational Physics" by Landau and Paez  *
-// *	       copyrighted by John Wiley and Sons, New York                  *
-// *                                                                      *
-// *  written by: students in PH465/565, Computational Physics,           *
-// *	       at Oregon State University                                    *
-// *              code copyrighted by RH Landau                           *
-// *  supported by: US National Science Foundation, Northwest Alliance    *
-// *                for Computational Science and Engineering (NACSE),    *
-// *                US Department of Energy 	                             *
-// *								                                                       *
-// *  UNIX (DEC OSF, IBM AIX): cc integ.c -lm                             *
-// *    			                                                             *
-// **************************************************************************/
-// {
-// int n;
-// double interval, sum=0.0, x;
-//
-// interval = ((max-min))/(no-1);
-//
-// for (n=2; n<no; n+=2)           /* loop for odd points      */
-//    {
-//        x = interval * (n-1);
-//        sum += 4 * Ixxint(x);
-//    }
-//    for (n=3; n<no; n+=2)        /* loop for even points     */
-//    {
-//       x = interval * (n-1);
-//       sum += 2 * Ixxint(x);
-//    }
-//    sum +=  Ixxint(min) + Ixxint(max);	 	  /* add first and last value */
-//    sum *= interval/3.;        	/* then multilpy by interval*/
-//
-//    return (sum);
-//
-// }
-// /* ********************************************************************* */
-// double Izzint(const Data *d, Grid *grid)
-// /*!
-//  *  Integrand for Izz moment of inertia
-//  *
-//  *
-//  *********************************************************************** */
-// {
-//   int i,j,k;
-//   double  *x1, *x2, *x3,rho,prs,dv;
-//
-//   x1 = grid->x[IDIR];
-//   x2 = grid->x[JDIR];
-//   x3 = grid->x[KDIR];
-//
-//   rho = d->Vc[RHO][k][j][i];
-//
-//   dv = (1/100.0)(CONST_PI/20.0)(CONST_PI/(40.0));
-//
-//   return ((rho*((x1[i]*sin(x2[j])*cos(x3[k])*(x1[i]*sin(x2[j])*sin(x3[k])))+
-//   ((x1[i]*sin(x2[j])*sin(x3[k]))*(x1[i]*sin(x2[j])*sin(x3[k]))))*x1[i]*x1[i]*sin(x2[j]))*dv);
-//
-// }
-// /* ********************************************************************* */
-// double Ixxint(const Data *d, Grid *grid)
-// /*!
-//  *  Integrand for Ixx moment of inertia
-//  *
-//  *
-//  *********************************************************************** */
-// {
-//   int i,j,k;
-//   double  *x1, *x2, *x3,rho,prs,dv;
-//
-//   x1 = grid->x[IDIR];
-//   x2 = grid->x[JDIR];
-//   x3 = grid->x[KDIR];
-//
-//   rho = d->Vc[RHO][k][j][i];
-//
-//   dv = (1/100.0)(CONST_PI/20.0)(CONST_PI/(40.0));
-//
-//   return ((rho*((x1[i]*sin(x2[j])*sin(x3[k])*(x1[i]*sin(x2[j])*sin(x3[k])))+
-//   ((x1[i]*cos(x2[j]))*(x1[i]*cos(x2[j]))))*x1[i]*x1[i]*sin(x2[j]))*dv);
-//
-// }
+
 /* ********************************************************************* */
 void Analysis (const Data *d, Grid *grid)
 /*!
@@ -406,15 +276,10 @@ Bt = 0.0;
      dV = x1[i]*x1[i]*sin(x2[j])*dr[i]*dt[j]*dp[k];
 
      if (rho > (VACUUM / UNIT_DENSITY) ){
-//
-//       //printf("Stuff is happening! %.5e\n",rho);
-//
-
-
        Bt = Bt + (d->Vc[BX3][k][j][i]*d->Vc[BX3][k][j][i])*dV;
        Izz = Izz +  d->Vc[PRS][k][j][i]*(pow(x1[i]*sin(x2[j])*sin(x3[k]),2)+pow(x1[i]*cos(x2[j]),2))*dV;
        Ixx = Ixx +  d->Vc[PRS][k][j][i]*(pow(x1[i]*sin(x2[j])*cos(x3[k]),2)+pow(x1[i]*sin(x2[j])*sin(x3[k]),2))*dV;
-//       fprintf(f,"Density at r =%f, theta = %f,phi = %f\n",x1[i],x2[j],x3[k]);
+/*!       fprintf(f,"Density at r =%f, theta = %f,phi = %f\n",x1[i],x2[j],x3[k]);
 //
 //       //fprintf(f,"%.5e\n",rho);
 //       // if (f == NULL ){
@@ -423,7 +288,7 @@ Bt = 0.0;
 //       // }
 //       // fprintf(f,"Radius: %12.6f Density: %12.6e  Pressure: %12.6e \n",x1[i],rho,prs);
 //
-//
+*/
  }
 }
 
