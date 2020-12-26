493e4cea9b6ece221b63d39408dde3a8dba7706b
samfrederick <samfred12@gmail.com>
2020-12-26 18:10:07 -0500

    Separate plotting modules for MOI and ellipticity. Plot timeseries with first and second derivs. Place code for copying over files to external drive in separate function.

--- Python Files/Ellipticity Analysis/MOI_analysis.py
+++ Python Files/Ellipticity Analysis/MOI_analysis.py
@@ -10,9 +10,12 @@ import os
 import shutil as sh
 import matplotlib.pyplot as plt
 import seaborn as sns
+import math as mt
 from datetime import datetime
 sns.set_style('darkgrid')
 
+data_path = '/media/sam/BE48068348063B23/Simulation_Results/201206/'
+extern_drive_path = '/media/sam/ASTRO_DATA/201206/'
 
 def MOI_Import(filename):
     """
@@ -88,7 +91,7 @@ def Differentiate(df, column, time_column):
 
     # Merge derivative values to staging dataframe
     deriv_df = pd.DataFrame(index=df[idx_name], columns=['d' + column])
-    
+
     try:
         deriv_df.iloc[0, 0] = head_deriv
     except ValueError as e:
@@ -116,21 +119,35 @@ def Differentiate(df, column, time_column):
     return df
 
 
-def Plot_MOI_Ellip(df, savefig=False):
+def Plot_Ellip_Timeseries(df, savefig=False):
     """
     """
-    fig, axs = plt.subplots(2, 1, figsize=(9, 7))
+    fig, axs = plt.subplots(3, 1, figsize=(9, 7))
     plt.subplots_adjust(hspace=.35)
 
-    axs[0].set_title('Principal Moments of Inertia')
-    axs[0].plot(df.index, df.Ixx, df.index, df.Iyy, df.index, df.Izz)
-    axs[0].legend(labels=['Ixx', 'Iyy', 'Izz'])
+    time_lims = -0.5, mt.ceil(df.index[-1])
 
-    axs[1].set_title('Stellar Ellipticity')
-    axs[1].plot(df.index, df.ellip, c='purple')
-    axs[1].legend(labels=['Ellipticity'])
+    axs[0].set_title(r'Stellar Ellipticity ($\epsilon$)')
+    axs[0].plot(df.index, df.ellip, c='#aa4ab0')
+    axs[0].legend(labels=[r'$\epsilon$'])
+    axs[0].axhline(y=0, color='#949494', linestyle='--')
+    axs[0].set_ylim(-0.15, 0.15)
+    axs[0].set_xlim(time_lims)
+
+    axs[1].set_title('First Derivative')
+    axs[1].plot(df.index, df.d_ellip, c='#d6004c')
     axs[1].axhline(y=0, color='#949494', linestyle='--')
+    axs[1].legend(labels=[r'$\partial_t$ $\epsilon$'])
     axs[1].set_ylim(-0.15, 0.15)
+    axs[1].set_xlim(time_lims)
+
+    axs[2].set_title('Second Derivative')
+    axs[2].plot(df.index, df.d_d_ellip, c='#465aea')
+    axs[2].axhline(y=0, color='#949494', linestyle='--')
+    axs[2].legend(labels=[r'$\partial^2_t$ $\epsilon$'])
+    axs[2].set_ylim(-1.5, 1.5)
+    axs[2].set_xlim(time_lims)
+    axs[2].set_xlabel('Time (s)')
 
     if savefig:
         today = datetime.now().strftime('%Y%m%d_%H%M%S')
@@ -138,22 +155,62 @@ def Plot_MOI_Ellip(df, savefig=False):
         plt.close()
 
 
-df = MOI_Import('201206_InertiaTensor.csv')
-Plot_MOI_Ellip(df, savefig=True)
-df[['Ixx', 'Iyy', 'Izz']].plot()
-df[['d_Ixx', 'd_Iyy', 'd_Izz']].plot()
-df[['d_d_Ixx', 'd_d_Iyy', 'd_d_Izz']].plot()
+def Plot_MOI_Timeseries(df, savefig=False):
+    """
+    """
+    fig, axs = plt.subplots(3, 1, figsize=(9, 8))
+    plt.subplots_adjust(hspace=.35)
+    time_lims = -0.5, mt.ceil(df.index[-1])
+
+    axs[0].set_title('Principal Moments of Inertia I$_{xx}$ and I$_{zz}$')
+    axs[0].plot(df.index, df.Ixx, df.index, df.Izz)
+    axs[0].legend(labels=['I$_{xx}$', 'I$_{zz}$'])
+    axs[0].set_xlim(time_lims)
+
+    axs[1].set_title('First Derivative')
+    axs[1].plot(df.index, df.d_Ixx, df.index, df.d_Izz)
+    axs[1].legend(labels=[r'$\partial_t$ I$_{xx}$',
+                          r'$\partial_t$ I$_{zz}$'])
+    axs[1].axhline(y=0, color='#949494', linestyle='--')
+    axs[1].set_ylim(-2.5e44, 2.5e44)
+    axs[1].set_xlim(time_lims)
+
+    axs[2].set_title('Second Derivative')
+    axs[2].plot(df.index, df.d_d_Ixx, df.index, df.d_d_Izz)
+    axs[2].legend(labels=[r'$\partial^2_t$ I$_{xx}$',
+                          r'$\partial^2_t$ I$_{zz}$'])
+    axs[2].axhline(y=0, color='#949494', linestyle='--')
+    axs[2].set_ylim(-2.5e45, 2.5e45)
+    axs[2].set_xlabel('Time (s)')
+    axs[2].set_xlim(time_lims)
+
+    if savefig:
+        today = datetime.now().strftime('%Y%m%d_%H%M%S')
+        plt.savefig('201206_MOI_'+today+'.png', dpi=300)
+        plt.close()
+
+
+def Copy_Data(path, destination):
+    """
+    """
+    # Copy every tenth data file from data directory to external drive
+    for cwd, folders, files in os.walk(data_path):
+        for filename in files:
+            if 'data' in filename:
+                file_n = int(filename.replace('data.', '').replace('.vtk', ''))
+                if file_n % 50 == 0:
+                    # Only write if file not in folder
+                    if not os.path.exists(extern_drive_path+filename):
+                        sh.copyfile(data_path+filename,
+                                    extern_drive_path+filename)
+
 
 # -----------------------------------------------------------------------------
-data_path = '/media/sam/BE48068348063B23/Simulation_Results/201206/'
-extern_drive_path = '/media/sam/ASTRO_DATA/201206/'
 
-# Copy every tenth data file from data directory to external drive
-for cwd, folders, files in os.walk(data_path):
-    for filename in files:
-        if 'data' in filename:
-            file_n = int(filename.replace('data.', '').replace('.vtk', ''))
-            if file_n % 50 == 0:
-                # Only write if file not in folder
-                if not os.path.exists(extern_drive_path+filename):
-                    sh.copyfile(data_path+filename, extern_drive_path+filename)
+Copy_Data(path=data_path, destination=extern_drive_path)
+
+df = MOI_Import('201206_InertiaTensor.csv')
+
+Plot_Ellip_Timeseries(df, savefig=True)
+
+Plot_MOI_Timeseries(df, savefig=True)
