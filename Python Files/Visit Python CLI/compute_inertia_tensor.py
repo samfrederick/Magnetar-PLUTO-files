9d31b69f63a93962e41d51bbe935a7b8124d4ebe
Sam Frederick <safrederick@davidson.edu>
2020-12-26 21:24:29 -0500

    Update compute_inertia_tensor.py
    Minor cosmetic updates, add variable for passing filename to function.

--- Python Files/Visit Python CLI/compute_inertia_tensor.py
+++ Python Files/Visit Python CLI/compute_inertia_tensor.py
@@ -9,8 +9,7 @@ Compute inertia tensor component values for stellar
 region defined by r <=R_star=1.0 on each timestep.
 
 """
-# Clear any existing plots
-DeleteAllPlots()
+AddWindow()
 
 print "Computing Inertia Tensor\n"
 
@@ -43,31 +42,39 @@ DrawPlots()
 # Get the number of time steps
 nstates = TimeSliderGetNStates()
 
-# Write inertia tensor to file in specified simulation steps
-# Function via http://visitusers.org/index.php?title=Query_over_time
-def DoublePrecisionQueryOverTime():
-    f = open("201206_InertiaTensor.csv", "w")
+
+def InertiaTensorQuery(filename):
+    """
+    Write inertia tensor to file in specified simulation
+    steps.
+
+    Function modified via:
+    http://visitusers.org/index.php?title=Query_over_time
+    """
+    f = open(filename, "w")
     f.write("t,Ixx,Ixy,Ixz,Iyx,Iyy,Iyz,Izx,Izy,Izz\n")
+
     for tstate in range(0, nstates):
-        TimeSliderSetState(tstate)
+     
         # Get data file time
+        TimeSliderSetState(tstate)
         Query("Time")
         t = GetQueryOutputValue()
+
         # Compute inertia tensor for data file
         Query("Moment of Inertia")
-        tensor = GetQueryOutputValue()
+        tsr = GetQueryOutputValue()  # tensor MOI values
      
         # Write time and inertia tensor to file
         str = "%4.3f,%25.15e,%25.15e,%25.15e,%25.15e,%25.15e,%25.15e,%25.15e,%25.15e,%25.15e\n" \
-	          %(t,tensor[0],tensor[1],tensor[2],tensor[3],
-                tensor[4],tensor[5],tensor[6],tensor[7],tensor[8])
+	          %(t,tsr[0],tsr[1],tsr[2],tsr[3],tsr[4],tsr[5],tsr[6],tsr[7],tsr[8])
         f.write(str)
-
+     
         # Print every tenth state to console
         if tstate % 10 == 0:
-            nt "3.2f" % t
+            print "t=%3.2f"%(t)
 
     f.close()
 
 
-DoublePrecisionQueryOverTime()
+InertiaTensorQuery(filename="201206_InertiaTensor.csv")
