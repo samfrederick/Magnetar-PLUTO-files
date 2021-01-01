29461c70ce12c637e960f4960f8490cdfbb98dfe
Sam Frederick <safrederick@davidson.edu>
2021-01-01 15:53:13 -0500

    Move differentiate function to separate module

--- Analysis/Ellipticity Analysis/MOI_analysis.py
+++ Analysis/Ellipticity Analysis/MOI_analysis.py
@@ -5,18 +5,22 @@ Created on Sun Dec  6 12:45:39 2020
 
 @author: Sam Frederick
 """
-import pandas as pd
 import os
+os.chdir('/Users/samfrederick/Documents/GitHub/Magnetar-PLUTO-files/Analysis')
+
+import pandas as pd
 import shutil as sh
 import matplotlib.pyplot as plt
 import seaborn as sns
 import math as mt
 from datetime import datetime
+from differentiate import TimeDerivative # Must set path to Analysis folder
 sns.set_style('darkgrid')
 
 data_path = '/media/sam/BE48068348063B23/Simulation_Results/201206/'
 extern_drive_path = '/media/sam/ASTRO_DATA/201206/'
 
+folder = 'Ellipticity Analysis'
 
 def MOI_Import(filename):
     """
@@ -48,81 +52,18 @@ def MOI_Import(filename):
     dataset['ellip'] = (dataset.Izz - dataset.Ixx)/(dataset.loc[0.0, 'Izz'])
 
     # Compute time derivative for ellipticity
-    dataset = Differentiate(dataset, column='ellip', time_column='t')
-    dataset = Differentiate(dataset, column='d_ellip', time_column='t')
+    dataset = TimeDerivative(dataset, column='ellip', time_column='t')
+    dataset = TimeDerivative(dataset, column='d_ellip', time_column='t')
 
     # Compute first time derivative of principal MOIs
     for moi in ['Ixx', 'Iyy', 'Izz']:
-        dataset = Differentiate(dataset, column=moi, time_column='t')
+        dataset = TimeDerivative(dataset, column=moi, time_column='t')
 
     # Compute second time derivative of principal MOIs
     for d_moi in ['d_Ixx', 'd_Iyy', 'd_Izz']:
-        dataset = Differentiate(dataset, column=d_moi, time_column='t')
+        dataset = TimeDerivative(dataset, column=d_moi, time_column='t')
 
     return dataset
-
-
-def Differentiate(df, column, time_column):
-    """
-    Compute the time derivative for pandas column data using the central
-    difference method and foward and backward schemes for endpoints.
-    """
-    idx_name = df.index.name
-    df = df.reset_index()
-    end_idx = len(df) - 1
-
-    # Backward diff for column tail
-    tail_data = df[[time_column, column]].tail(n=2)
-    tail_dt = (tail_data.loc[end_idx, time_column]
-               - tail_data.loc[end_idx-1, time_column])
-    tail_deriv = (1/tail_dt)*(tail_data.loc[end_idx, column]
-                              - tail_data.loc[end_idx-1, column])
-
-    # Forward diff for column head
-    head_data = df[[time_column, column]].head(n=2)
-    head_dt = (head_data.loc[1, time_column]
-               - head_data.loc[0, time_column])
-    head_deriv = (1/head_dt)*(head_data.loc[1, column]
-                              - head_data.loc[0, column])
-
-    # Central diff for column body
-    central_data = df.loc[1:end_idx-1, [time_column, column]]
-    central_dt = pd.Series(central_data[1:][time_column].values
-                           - central_data[:-1][time_column].values)
-    central_diff = pd.Series(central_data[1:-1][column].values
-                             - central_data[0:-2][column].values)
-    central_deriv = (1/central_dt)*central_diff
-    central_deriv.index = central_data[0:-1][time_column]
-
-    # Merge derivative values to staging dataframe
-    deriv_df = pd.DataFrame(index=df[idx_name], columns=['d' + column])
-
-    try:
-        deriv_df.iloc[0, 0] = head_deriv
-    except ValueError as e:
-        print('head:', e)
-        pass
-
-    try:
-        deriv_df.iloc[-1, 0] = tail_deriv
-    except ValueError as e:
-        print('tail:', e)
-        pass
-
-    try:
-        # Remove issue at t = 0.45 with duplicated index
-        central_deriv = central_deriv[~central_deriv.index.duplicated()]
-        deriv_df.iloc[1:-1, 0] = central_deriv[:]
-    except ValueError as e:
-        print('body:', e)
-        pass
-
-    # Join derivative column with passed dataframe
-    df = df.set_index(idx_name, drop=True)
-    df['d_'+column] = deriv_df['d' + column]
-
-    return df
-
 
 def Plot_Ellip_Timeseries(df, savefig=False):
     """
@@ -156,8 +97,8 @@ def Plot_Ellip_Timeseries(df, savefig=False):
 
     if savefig:
         today = datetime.now().strftime('%Y%m%d_%H%M%S')
-        plt.savefig('201206_MOI_ellip_'+today+'.png', dpi=300)
-        plt.close()
+        plt.savefig(folder + '/201206_MOI_ellip_'+today+'.png', dpi=300)
+        #plt.close()
 
 
 def Plot_MOI_Timeseries(df, savefig=False):
@@ -191,8 +132,8 @@ def Plot_MOI_Timeseries(df, savefig=False):
 
     if savefig:
         today = datetime.now().strftime('%Y%m%d_%H%M%S')
-        plt.savefig('201206_MOI_'+today+'.png', dpi=300)
-        plt.close()
+        plt.savefig(folder + '/201206_MOI_'+today+'.png', dpi=300)
+        #plt.close()
 
 
 def Copy_Data(path, destination):
@@ -212,9 +153,9 @@ def Copy_Data(path, destination):
 
 # -----------------------------------------------------------------------------
 
-Copy_Data(path=data_path, destination=extern_drive_path)
+#Copy_Data(path=data_path, destination=extern_drive_path)
 
-df = MOI_Import('201206_InertiaTensor.csv')
+df = MOI_Import(folder +'/201206_InertiaTensor.csv')
 
 Plot_Ellip_Timeseries(df, savefig=True)
 
