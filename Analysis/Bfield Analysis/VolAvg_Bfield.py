5cb93303c6bfee5b653725677d119a6dc7bafaa8
Sam Frederick <safrederick@davidson.edu>
2021-01-17 19:43:27 -0500

    Update figure dimensions, font size

--- Analysis/Bfield Analysis/VolAvg_Bfield.py
+++ Analysis/Bfield Analysis/VolAvg_Bfield.py
@@ -15,6 +15,8 @@ from datetime import datetime
 from differentiate import TimeDerivative # Must set path to Analysis folder
 import math as mt
 sns.set_style('darkgrid')
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
 
 def ImportBfieldData():
     total = pd.read_csv('Bfield Analysis/201206_VolAvg_Total_Bfield.csv',
@@ -37,36 +39,52 @@ def ImportBfieldData():
     return bfield_df
     
 def PlotBfield(df):    
-    fig, axs = plt.subplots(3, 1, figsize=(9, 7))
-    plt.subplots_adjust(hspace=.35)
+    fsize = 12
+    fig, axs = plt.subplots(3, 1, figsize=(6, 7))
     
     time_lims = -0.5, mt.ceil(bfield_df.index[-1])
     
-    #axs[0].set_title(r'Stellar Ellipticity ($\epsilon$)')
+    comp_names = ['BPoloidal_v_avg', 'BToroidal_v_avg']
+    d_comp_names = ['d_BPoloidal_v_avg', 'd_BToroidal_v_avg']
+    dd_comp_names = ['d_d_BPoloidal_v_avg', 'd_d_BToroidal_v_avg']
+    
+    axs[0].set_title('Volume-averaged Component Fields', fontsize=fsize)
     axs[0].plot(df.index,
-                df[['BTotal_v_avg', 'BPoloidal_v_avg', 'BToroidal_v_avg']])
-    axs[0].legend(labels=[r'$B_{total}$', '$B_{p}$', '$B_{t}$'], loc='upper right',
-                  fontsize=8, framealpha=.5)
-    #axs[0].set_ylim(-2e17, 5.1e17)
-    axs[0].set_xlim(time_lims)
+                df[comp_names],
+                linewidth=2)
+    axs[0].legend(labels=['$\\bar{B}_{p}$', '$\\bar{B}_{t}$'],
+                  loc='upper right', fontsize=10, framealpha=1)
+    axs[0].set_xlim(-1, 22)
+    axs[0].tick_params(axis='both', labelsize=11) 
     
-    axs[1].set_title('First Derivative')
+    axs[1].set_title('First Derivative', fontsize=fsize)
     axs[1].plot(df.index,
-                df[['d_BTotal_v_avg', 'd_BPoloidal_v_avg', 'd_BToroidal_v_avg']])
+                df[d_comp_names],
+                linewidth=2)
     axs[1].axhline(y=0, color='#949494', linestyle='--')
-    #axs[1].legend(labels=[r'$\partial_t$ $\epsilon$'], loc='lower right')
+    axs[1].legend(labels=['$\partial_t$ $\\bar{B}_{p}$',
+                          '$\partial_t$ $\\bar{B}_{t}$'],
+                  loc='upper right',fontsize=10)
     axs[1].set_ylim(-2e17, 2e17)
-    axs[1].set_xlim(time_lims)
+    axs[1].set_xlim(-1, 22)
+    axs[1].tick_params(axis='both', labelsize=11) 
     
-    axs[2].set_title('Second Derivative')
+    axs[2].set_title('Second Derivative', fontsize=fsize)
     axs[2].plot(df.index, 
-                df[['d_d_BTotal_v_avg', 'd_d_BPoloidal_v_avg', 'd_d_BToroidal_v_avg']])
+                df[dd_comp_names],
+                linewidth=2)
     axs[2].axhline(y=0, color='#949494', linestyle='--')
-    #axs[2].legend(labels=[r'$\partial^2_t$ $\epsilon$'], loc='lower right')
+    axs[2].legend(labels=['$\partial^2_t$ $\\bar{B}_{p}$',
+                          '$\partial^2_t$ $\\bar{B}_{t}$'],
+                  loc='upper right',fontsize=10)
     axs[2].set_ylim(-1e18, 1e18)
-    axs[2].set_xlim(time_lims)
-    axs[2].set_xlabel('Time (s)')
+    axs[2].set_xlim(-1, 22)
+    axs[2].set_xlabel('Time (s)', fontsize=fsize)
+    axs[2].tick_params(axis='both', labelsize=11) 
     
+    plt.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top = 0.9, hspace=.5)
+    plt.tight_layout()
+
     today = datetime.now().strftime('%Y%m%d_%H%M%S')
     plt.savefig('Bfield Analysis/201206_Volume_Avg_Bfield_'+today+'.png', dpi=300)
     #plt.close()
